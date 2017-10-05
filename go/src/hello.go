59263454cdedb75d294737716b2500fd058cfe9b
Simon from Mac <lis@nextechat.com>
2017-10-05 16:11:38 +0800

    clean up

--- go/src/hello.go
+++ go/src/hello.go
@@ -13,8 +13,7 @@ import (
 )
 
 func main() {
-	fmt.Printf("hello, world\n")
-	// Listen on a random endpoint.
+	// Listen on an endpoint.
 	endpoint := fmt.Sprintf("secrettunnel")
 	if runtime.GOOS == "windows" {
 		endpoint = `\\.\pipe\` + endpoint
@@ -36,93 +35,57 @@ func main() {
 	}
 }
 
-func write2js(msgWriter *bufio.Writer, go2jsMessage <-chan string) {
-	for {
-		msg := <-go2jsMessage
-		fmt.Println("write to js: " + msg)
-		msgWriter.Write([]byte(msg))
-		msgWriter.Flush()
-	}
-}
-
 func byteArr2Uint(p []byte) uint32 {
     data := binary.BigEndian.Uint32(p)
 	fmt.Println(data)
 	return data
 }
-func uint2ByteArr(val uint32) []byte {
-	bs := make([]byte, 4)
-    binary.BigEndian.PutUint32(bs, val)
-	fmt.Println(bs)
-	return bs
-}
-// func main() {
-// 	fmt.Println("Hello, playground")
-// 	sss := sizeByteToSize([]byte{byte(0),byte(0),byte(3),byte(231)})
-// 	fmt.Println(sss)
-// }
+
 // Handles incoming requests.
 func handleRequest(conn net.Conn) {
 	msgReader := bufio.NewReader(conn)
 	msgWriter := bufio.NewWriter(conn)
-	//rw := bufio.NewReadWriter(bufio.NewReader(conn), bufio.NewWriter(conn))
 	defer conn.Close()
 
-	go2jsMessage := make(chan string)
-	_, err := msgWriter.WriteString("channel set up!")
-	if err != nil {
-		fmt.Println("Cannot write to connection.\n", err)
+	sizebytes := make([]byte, 4)
+	line, err := msgReader.Read(sizebytes)
+	switch {
+	case err == io.EOF:
+		fmt.Println("Reached EOF - close this connection.")
+		return
+	case err != nil:
+		//fmt.Println("\nError reading command. Got: '"+line+"'\n", err)
+		if err != nil {
+			fmt.Println("Cannot write to connection.\n", err)
+		}
 		return
 	}
-	msgWriter.Flush()
-	go write2js(msgWriter, go2jsMessage)
-
-readLoop:
-	for {
-		//line, err := msgReader.ReadString('\n')
-		sizebytes := make([]byte, 4)
-		line, err := msgReader.Read(sizebytes)
-		switch {
-		case err == io.EOF:
-			fmt.Println("Reached EOF - close this connection.")
-			break readLoop
-		case err != nil:
-			//fmt.Println("\nError reading command. Got: '"+line+"'\n", err)
-			if err != nil {
-				fmt.Println("Cannot write to connection.\n", err)
-			}
-			break readLoop
+	fmt.Println(line)
+	bodyLen := byteArr2Uint(sizebytes)
+	bodyBytes := make([]byte, bodyLen)
+	_, err2 := msgReader.Read(bodyBytes)
+	switch {
+	case err2 == io.EOF:
+		fmt.Println("Reached EOF - close this connection.")
+		return
+	case err2 != nil:
+		//fmt.Println("\nError reading command. Got: '"+line+"'\n", err)
+		if err2 != nil {
+			fmt.Println("Cannot write to connection.\n", err2)
 		}
-		fmt.Println(line)
-		bodyLen := byteArr2Uint(sizebytes)
-		bodyBytes := make([]byte, bodyLen)
-		_, err2 := msgReader.Read(bodyBytes)
-		switch {
-		case err2 == io.EOF:
-			fmt.Println("Reached EOF - close this connection.")
-			break readLoop
-		case err2 != nil:
-			//fmt.Println("\nError reading command. Got: '"+line+"'\n", err)
-			if err2 != nil {
-				fmt.Println("Cannot write to connection.\n", err2)
-			}
-			break readLoop
-		}
-		// one piece of message received
-		message := string(bodyBytes[:])
-		handleStrings(message, go2jsMessage)
+		return
 	}
+	// one piece of message received
+	message := string(bodyBytes[:])
+	handleStrings(message, msgWriter)
 
 }
-func handleStrings(cmd string, go2jsMessage chan<- string) {
+func handleStrings(cmd string, msgWriter *bufio.Writer) {
 	fmt.Println("handle: " + cmd)
-	go2jsMessage <- "Thank you." + cmd
-	// if err != nil {
-	// 	fmt.Println("Cannot write to connection.\n", err)
-	// 	return
-	// }
-	// err = rw.Flush()
-	// if err != nil {
-	// 	fmt.Println("Flush failed.", err)
-	// }
+	longlongs := make([]byte, 999)
+	for i:= range longlongs {
+		longlongs[i] = 'b'
+	}
+	msgWriter.Write(longlongs)
+	msgWriter.Flush()
 }
--- nodejs/src/index.js
+++ nodejs/src/index.js
@@ -11,34 +11,18 @@ if(platform == 'win32') {
     PIPE_PATH = _.trimEnd(os.tmpdir(), '/') + '/' + PIPE_NAME
 }
 
-function Uint32ToUint8Arr(num) {
-
-    arr = new ArrayBuffer(4); // an Int32 takes 4 bytes
-    view = new DataView(arr);
-    view.setUint32(0, num, false); // byteOffset = 0; litteEndian = false
-    return arr;
-}
-
-function Uint8ArrToInt(Uint8Arr) {
-    var length = Uint8Arr.length;
-
-    let buffer = Buffer.from(Uint8Arr);
-    var result = buffer.readUIntBE(0, length);
-
-    return result;
-}
-
 var client = net.connect(PIPE_PATH, function() {
     console.log('Client: on connection');
 })
 
+var msg = ''
 client.on('data', function(data) {
-    console.log('Client: on data:', data.toString());
-    //client.end('Thanks!');
+    msg += data
 });
 
 client.on('end', function() {
     console.log('Client: on end');
+    console.log(msg);
 })
 
 function MsgToBuf(msg) {
