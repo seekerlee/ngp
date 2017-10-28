de06edecbd29ee43c07c192d8c7aff3848136ca7
Simon <lishi1608@gmail.com>
2017-10-28 17:24:42 +0800

    fix

--- nodejs/src/index.js
+++ nodejs/src/index.js
@@ -2,3 +2,16 @@ const SendMsg = require('./pipe/pipe.js');
 SendMsg('haha', function(response){
     console.log(response)
 })
+
+client.on('data', function(data) {
+    console.log('Client: on data:', data.toString());
+    client.end('Thanks!');
+});
+
+client.on('end', function() {
+    console.log('Client: on end');
+})
+
+//client.write('Hello, server! Love, Client.\n');
+//client.write('2222\n');
+client.write('1\n');
